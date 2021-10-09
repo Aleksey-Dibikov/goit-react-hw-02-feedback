593e8f507a2aa33073ccc45cecb6fc2cfb906aab
Aleksey Dibikov <dibikovaleksey@gmail.com>
2021-10-10 01:47:42 +0300

    Updates

--- static/js/main.24a1e7b5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),o=c(3),i=c.n(o),a=(c(8),c(9),c(0));var s=function(){return Object(a.jsx)("div",{className:"App",children:"home work"})};i.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
-//# sourceMappingURL=main.24a1e7b5.chunk.js.map
